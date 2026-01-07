431827626a375b702adb6827952cefa527a9b1f2
MortazaMaruf <mh13118216@gmail.com>
2026-01-07 12:44:59 +0600

    cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useLayoutEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { div } from 'framer-motion/client';
+
 const Cursor = () => {
        const cursorRef =  useRef(null)
         useLayoutEffect(()=> {
@@ -11,8 +11,9 @@ const Cursor = () => {
                 y:dts.y,
                 duration:0.5,
                 delay:0.04,
-                ease:"elastic.out(1.2,0.4)",
+                ease:"power3.out",
                 opacity:1,
+                skew:20
                 
             })
           }
@@ -22,7 +23,8 @@ const Cursor = () => {
                 delay:.04
             })
           };
-          
+             document.body.addEventListener("mousemove",cursorMove);
+          document.body.addEventListener("mouseleave",cursorLeave);
           return ()=>{
             document.body.addEventListener("mousemove",cursorMove);
           document.body.addEventListener("mouseleave",cursorLeave);
@@ -34,7 +36,7 @@ const Cursor = () => {
         
         <div 
      ref={cursorRef}
-    className='fixed z-100 w-4 h-4 opacity-0 bg-[#DF5E04] rounded-full'>
+    className='fixed z-50 w-4 h-4 opacity-0 bg-[#DF5E04] rounded-full pointer-events-none'>
       
     </div>
     </div>
